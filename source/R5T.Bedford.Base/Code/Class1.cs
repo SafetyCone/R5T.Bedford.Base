5554588affe7e43977a21f249160a56949c5aa52
MinexAutomation <minex.automation@gmail.com>
2020-01-25 01:13:44 -0800

    Added IFileEqualityComparer service definition interface type.

--- source/R5T.Bedford.Base/Code/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace R5T.Bedford.Base
-{
-    public class Class1
-    {
-    }
-}
---
+++ source/R5T.Bedford.Base/Code/Services/Definitions/IFileEqualityComparer.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+
+namespace R5T.Bedford
+{
+    public interface IFileEqualityComparer
+    {
+        bool Equals(string filePath1, string filePath2);
+    }
+}
